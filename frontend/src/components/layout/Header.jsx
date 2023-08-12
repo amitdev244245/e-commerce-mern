4d6029fe04e255a964df388a0232348a7310ccf8
Amit Sharma <amitdev244245@gmail.com>
2023-08-13 04:12:19 +0530

    Designed the header

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,32 +1,31 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import { GiShoppingCart } from 'react-icons/gi'
 
 const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">E-Commerce</a>
+                <Link to="/" className="navbar-brand fw-bold"> <GiShoppingCart size={30} />E-Commerce</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <NavLink to="/" className="nav-link active" aria-current="page">Home</NavLink>
+                        <li className="nav-item mx-2">
+                            <NavLink to="/" className="nav-link" aria-current="page">Home</NavLink>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item mx-2">
                             <NavLink to="/about" className="nav-link">About</NavLink>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item mx-2">
                             <NavLink to="/contact" className="nav-link">Contact</NavLink>
                         </li>
-                        <li className="nav-item dropdown">
-                            <NavLink className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Account</NavLink>
-                            <ul className="dropdown-menu">
-                                <li><NavLink to="/login" className="dropdown-item">Login</NavLink></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><NavLink to="/signup" className="dropdown-item">Signup</NavLink></li>
-                            </ul>
+                        <li className="nav-item mx-2">
+                            <NavLink to="/login" className="nav-link">Login</NavLink>
+                        </li>
+                        <li className="nav-item mx-2">
+                            <NavLink to="/signup" className="nav-link">Signup</NavLink>
                         </li>
                     </ul>
                 </div>
