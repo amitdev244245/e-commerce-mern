e7091aa7bba4382e9e5c6e677de6173f138d4045
Amit Sharma <amitdev244245@gmail.com>
2023-08-13 05:18:54 +0530

    Designed the 404 page

--- frontend/src/components/pages/Page404.jsx
+++ frontend/src/components/pages/Page404.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Layout from '../layout/Layout'
+import { Link } from 'react-router-dom'
 
 const Page404 = () => {
     return (
         <Layout>
-            <h1>Page 404</h1>
+            <div className="p404 d-flex flex-column align-items-center justify-content-center">
+                <h1 className="p404-title fw-bolder">404</h1>
+                <h2>Ooops! Page Not Found</h2>
+                <Link to="/" className="p404-btn text-dark text-decoration-none border border-dark border-2 rounded p-2 mt-3">Go Back</Link>
+            </div>
         </Layout>
     )
 }
